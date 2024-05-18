7fb9f481be86f28e8f837c33c55fe281617efd16
minji1nOnly <tapawan.306172@dasmarinas.sti.edu.ph>
2024-05-18 13:14:28 +0800

    update2

---
+++ app/src/main/java/com/jtdev/rtsm/bldg_main2nd.java
@@ -0,0 +1,14 @@
+package com.jtdev.rtsm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bldg_main2nd extends AppCompatActivity {
+
+    @Override
+    protected void  onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bldg_main2ndfloor);
+    }
+}
