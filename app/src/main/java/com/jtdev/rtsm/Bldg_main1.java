4ea12070c8a047e1a16149e9e9ae6764ef37dd70
minji1nOnly <tapawan.306172@dasmarinas.sti.edu.ph>
2024-06-17 17:23:02 +0800

    DATE / TIME/ SCHED ADDED

--- app/src/main/java/com/jtdev/rtsm/Bldg_main1.java
+++ app/src/main/java/com/jtdev/rtsm/Bldg_main1.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+<<<<<<< Updated upstream
 import android.widget.ImageView;
+=======
+import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Calendar;
+>>>>>>> Stashed changes
 
 public class Bldg_main1 extends AppCompatActivity {
     ImageView Com_lab3;
+
+    ImageView comlab3;
+    TextView comlab3_status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +29,57 @@ public class Bldg_main1 extends AppCompatActivity {
         setContentView(R.layout.activity_bldg_main1);
         Com_lab3 = findViewById(R.id.ComLab3Button);
 
+        comlab3 = findViewById(R.id.Comlab3_Button);
+        comlab3_status = findViewById(R.id.Comlab3_Status);
         Button Floor2_1 = findViewById(R.id.Floor2_1);
         Button Floor2_2 = findViewById(R.id.Floor2_2);
+
+        comlab3_status = findViewById(R.id.Comlab3_Status);
+
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR); // 12-hour format
+        int minute = calendar.get(Calendar.MINUTE);
+        int am_pm = calendar.get(Calendar.AM_PM);
+
+        String amPm;
+        if (am_pm == Calendar.AM) {
+            amPm = "AM";
+        } else {
+            amPm = "PM";
+        }
+
+        String currentTime = (hour + ":" + minute + " " + amPm);
+
+        comlab3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (compareTime(12, 13)) {
+                    Toast.makeText(Bldg_main1.this, "This room is currently VACANT", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Bldg_main1.this, "A class is ongoing in this room.", Toast.LENGTH_SHORT).show();
+                }
+                if (compareTime(16, 24)){
+                    Toast.makeText(Bldg_main1.this, "This room is currently VACANT", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(Bldg_main1.this, "A class is ongoing in this room.", Toast.LENGTH_SHORT).show();
+                }
+                AlertDialog.Builder comlab3Message = new AlertDialog.Builder(Bldg_main1.this);
+                comlab3Message.setTitle("Computer Laboratory 2");
+                StringBuilder comlab3_content = new StringBuilder();
+                comlab3_content.append("Schedule                           SUBJECT\n");
+                comlab3_content.append("7:00AM - 10:00AM          HCI\n");
+                comlab3_content.append("12:00AM - 1:00PM          VACANT\n");
+                comlab3_content.append("1:00PM - 4:00PM             FMP\n");
+                comlab3_content.append("\nCurrent Time: ");
+                comlab3_content.append(currentTime);
+                comlab3Message.setMessage(comlab3_content);
+                comlab3Message.show();
+
+            }
+        });
+
+
+
         Floor2_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +95,7 @@ public class Bldg_main1 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+<<<<<<< Updated upstream
     Com_lab3.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -46,5 +107,31 @@ public class Bldg_main1 extends AppCompatActivity {
     });
 
 
+=======
+
+        updateTime();
+>>>>>>> Stashed changes
+    }
+    public boolean compareTime(int time1, int time2) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY); // 24-hour format
+
+        return hour >= time1 && hour < time2;
+    }
+    void updateTime(){
+        if (compareTime(12, 13)){
+            comlab3_status.setText("Available");
+            comlab3_status.setTextColor(Color.GREEN);
+        }else{
+            comlab3_status.setText("Occupied");
+            comlab3_status.setTextColor(Color.RED);
+        if (compareTime(16,24)){
+            comlab3_status.setText("Available");
+            comlab3_status.setTextColor(Color.GREEN);
+        }else{
+            comlab3_status.setText("Occupied");
+            comlab3_status.setTextColor(Color.RED);
+        }
+        }
     }
 }
--- app/src/main/java/com/jtdev/rtsm/bldg_main2nd.java
+++ app/src/main/java/com/jtdev/rtsm/bldg_main2nd.java
@@ -1,21 +1,71 @@
 package com.jtdev.rtsm;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class bldg_main2nd extends AppCompatActivity {
+    ImageView room202;
+    TextView room202_Status;
 
     @Override
     protected void  onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bldg_main2ndfloor);
 
+        room202 = findViewById(R.id.room202_Button);
+        room202_Status = findViewById(R.id.room202_Status);
         Button Floor2_1 = findViewById(R.id.Floor1);
         Button Floor2_2 = findViewById(R.id.Floor2_3);
+
+        room202_Status = findViewById(R.id.room202_Status);
+
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR); // 12-hour format
+        int minute = calendar.get(Calendar.MINUTE);
+        int am_pm = calendar.get(Calendar.AM_PM);
+
+        String amPm;
+        if (am_pm == Calendar.AM) {
+            amPm = "AM";
+        } else {
+            amPm = "PM";
+        }
+
+        String currentTime = (hour + ":" + minute + " " + amPm);
+
+        room202.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (compareTime(16, 18)) {
+                    Toast.makeText(bldg_main2nd.this, "A class is ongoing in this room.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(bldg_main2nd.this, "This room is currently VACANT.", Toast.LENGTH_SHORT).show();
+                }
+                AlertDialog.Builder comlab3Message = new AlertDialog.Builder(bldg_main2nd.this);
+                comlab3Message.setTitle("Room 202");
+                StringBuilder room202_content = new StringBuilder();
+                room202_content.append("Schedule                           SUBJECT\n");
+                room202_content.append("12:00AM - 4:00PM          VACANT\n");
+                room202_content.append("4:00PM - 6:00PM             FMP\n");
+                room202_content.append("\nCurrent Time: ");
+                room202_content.append(currentTime);
+                comlab3Message.setMessage(room202_content);
+                comlab3Message.show();
+
+            }
+        });
+
         Floor2_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,4 +81,29 @@ public class bldg_main2nd extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }}
+
+updateTime();
+    }
+public boolean compareTime(int time1, int time2) {
+    Calendar calendar = Calendar.getInstance();
+    int hour = calendar.get(Calendar.HOUR_OF_DAY); // 24-hour format
+
+    return hour >= time1 && hour < time2;
+}
+void updateTime(){
+    if (compareTime(12, 4)){
+        room202_Status.setText("Available");
+        room202_Status.setTextColor(Color.GREEN);
+    }else{
+        room202_Status.setText("Occupied");
+        room202_Status.setTextColor(Color.RED);
+        if (compareTime(18,24)){
+            room202_Status.setText("Available");
+            room202_Status.setTextColor(Color.GREEN);
+        }else{
+            room202_Status.setText("Occupied");
+            room202_Status.setTextColor(Color.RED);
+        }
+    }
+}
+}
